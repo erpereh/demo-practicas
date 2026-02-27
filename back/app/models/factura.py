9154e9bb3912fda35ca9d97246b85880f347f70f
Juan Carlos Alonso <psp.dam2.alonso@gmail.com>
2026-02-27 10:36:59 +0100

    Clase 'factura' actualizada
    Se ha modificado la clase factura según su tabla 'FACTURAS' de la base de datos

--- back/app/models/factura.py
+++ back/app/models/factura.py
@@ -1,9 +1,17 @@
-class Factura:
-    def __init__(self, id_sociedad, id_cliente, num_factura, fec_factura, concepto, base_imponible, total):
-        self.id_sociedad = id_sociedad
-        self.id_cliente = id_cliente
-        self.num_factura = num_factura
-        self.fec_factura = fec_factura
-        self.concepto = concepto
-        self.base_imponible = base_imponible
-        self.total = total
+from sqlalchemy import Column, String, Date, Text, ForeignKey, Numeric
+from app.database import Base
+
+class Factura(Base):
+    __tablename__ = "FACTURAS" #Tabla FACTURAS del SQL:
+
+    id_sociedad = Column("ID_SOCIEDAD", String(5), nullable=False, index=True) #Ej: 01
+    #id_cliente es una clave foránea con CLIENTES
+    id_cliente = Column("ID_CLIENTE", String(20), ForeignKey("CLIENTES.ID_CLIENTE"), nullable=False, index=True) #CYC
+
+    num_factura = Column("NUM_FACTURA", String(20), primary_key=True, index=True) #QS260001
+    fec_factura = Column("FEC_FACTURA", Date, nullable=False) #2026-01-31
+
+    concepto = Column("CONCEPTO", Text, nullable=False) #CYCPR00022 RRHH - Portal empleado...
+
+    base_imponible = Column("BASE_IMPONIBLE", Numeric(10, 2), nullable=False) #472.50
+    total = Column("TOTAL", Numeric(10, 2), nullable=False) #571.73
