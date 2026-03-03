13e9ce4cf4013eba554079233a2507e3e0e59734
Henry <henrybmsw@gmail.com>
2026-03-03 13:05:00 +0100

    Ajuste en Tarifas
    Ajuste en Tarifas y comentario en hist_proyecto

--- back/app/models/hist_proyecto.py
+++ back/app/models/hist_proyecto.py
@@ -2,16 +2,29 @@ from sqlalchemy import Column, String, Date, Numeric, ForeignKey
 from app.database import Base
 
 class HistProyecto(Base):
+    # Tabla HIST_PROYECTOS
     __tablename__ = "HIST_PROYECTOS"
 
+    # Sociedad
     id_sociedad = Column("ID_SOCIEDAD", String(10), primary_key=True)
 
-    id_empleado = Column("ID_EMPLEADO", String(20), ForeignKey("EMPLEADOS.ID_EMPLEADO"), primary_key=True, index=True)
+    # Empleado
+    id_empleado = Column("ID_EMPLEADO", String(20),
+                         ForeignKey("EMPLEADOS.ID_EMPLEADO"),
+                         primary_key=True, index=True)
 
-    id_cliente = Column("ID_CLIENTE", String(50), ForeignKey("CLIENTES.ID_CLIENTE"), primary_key=True, index=True)
+    # Cliente
+    id_cliente = Column("ID_CLIENTE", String(50),
+                        ForeignKey("CLIENTES.ID_CLIENTE"),
+                        primary_key=True, index=True)
 
-    id_proyecto = Column("ID_PROYECTO", String(50), ForeignKey("PROYECTOS.ID_PROYECTO"), primary_key=True, index=True )
+    # Proyecto
+    id_proyecto = Column("ID_PROYECTO", String(50),
+                         ForeignKey("PROYECTOS.ID_PROYECTO"),
+                         primary_key=True, index=True)
 
+    # Fecha inicio
     fec_inicio = Column("FEC_INICIO", Date, nullable=False)
 
+    # Tarifa
     tarifa = Column("TARIFA", Numeric(10, 2), nullable=False)
